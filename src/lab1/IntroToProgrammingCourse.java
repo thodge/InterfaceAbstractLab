c5aa5b23b1c312ef124b2711aa5cac33ab4f3bbd
Tammie <Tammie@10.10.10.104>
2014-02-02 16:02:58 -0600

    Fixed super class and extended sub classes

--- src/lab1/IntroToProgrammingCourse.java
+++ src/lab1/IntroToProgrammingCourse.java
@@ -3,13 +3,14 @@ package lab1;
 import javax.swing.JOptionPane;
 
 /**
- * Describe responsibilities here. NOTE: this class has NO PREREQUISITES!
- * Do not change this fact.
+ * Describe responsibilities here. NOTE: this class has NO PREREQUISITES! Do not
+ * change this fact.
  *
- * @author      your name goes here
- * @version     1.00
+ * @author your name goes here
+ * @version 1.00
  */
-public class IntroToProgrammingCourse {
+public class IntroToProgrammingCourse extends WctcCourse {
+
     private String courseName;
     private String courseNumber;
     private double credits;
@@ -24,7 +25,7 @@ public class IntroToProgrammingCourse {
     }
 
     public final void setCourseNumber(String courseNumber) {
-        if(courseNumber == null || courseNumber.length() == 0) {
+        if (courseNumber == null || courseNumber.length() == 0) {
             JOptionPane.showMessageDialog(null,
                     "Error: courseNumber cannot be null of empty string");
             System.exit(0);
@@ -37,7 +38,7 @@ public class IntroToProgrammingCourse {
     }
 
     public void setCredits(double credits) {
-        if(credits < 0.5 || credits > 4.0) {
+        if (credits < 0.5 || credits > 4.0) {
             JOptionPane.showMessageDialog(null,
                     "Error: credits must be in the range 0.5 to 4.0");
             System.exit(0);
@@ -50,7 +51,7 @@ public class IntroToProgrammingCourse {
     }
 
     public final void setCourseName(String courseName) {
-        if(courseName == null || courseName.length() == 0) {
+        if (courseName == null || courseName.length() == 0) {
             JOptionPane.showMessageDialog(null,
                     "Error: courseName cannot be null of empty string");
             System.exit(0);
@@ -58,5 +59,4 @@ public class IntroToProgrammingCourse {
         this.courseName = courseName;
     }
 
-    
 }
