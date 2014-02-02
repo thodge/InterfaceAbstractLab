222cc49d8b95c634d20fcfe0e499e04dbbe2c0c7
Tammie <Tammie@10.10.10.104>
2014-02-02 17:41:00 -0600

    created interface in lab 2 and implemented the classes

--- src/lab2/IntroJavaCourse.java
+++ src/lab2/IntroJavaCourse.java
@@ -6,7 +6,7 @@ package lab2;
  * @author      your name goes here
  * @version     1.00
  */
-public class IntroJavaCourse {
+public class IntroJavaCourse implements IwctcCourses{
     String courseName;
     private String courseNumber;
     private double credits;
